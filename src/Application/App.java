71dc4d9a4cfed86dae5094c500c610c16c666561
Gabriel Holanda <gabriel_ebot@hotmail.com>
2019-05-29 00:20:06 -0300

    Correções e Fixes

--- src/Application/App.java
+++ src/Application/App.java
@@ -6,20 +6,26 @@
 package Application;
 
 import Views.Principal;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  *
  * @author HOLANDAS
  */
-// Este será nosso MAIN
 public class App {
+
     public static void main(String[] args) {
-        // Programação defensiva
-        try{
-        Principal principal = new Principal();
-        principal.setVisible(true);
-        } catch (RuntimeException e){
-            e.getMessage();
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
         }
+
+        Principal p = new Principal();
+        p.setVisible(true);
     }
+
 }
